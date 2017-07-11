bd139273c59718ff2b24a21aa3c465aeca6453ee
Brian Sumner <brian.sumner@dot.gov>
2017-07-11 09:59:46 -0400

    Initial commit

---
+++ Scripts/cacoparking.js
@@ -0,0 +1,127 @@
+function requestData() {
+    // Dummy data for testing until the API is up
+    var data = [
+        {
+            "id": 19,
+            "parkAbbrevId": "CACO",
+            "abbrevId": "NAUSETLIGHT",
+            "name": "Nauset Light",
+            "lattitude": 41.858862,
+            "longitude": -69.951859,
+            "capacity": 167,
+            "note": "",
+            "status": "Open",
+            "statusTimeStamp": "2017-06-26T13:26:58.543886",
+            "freeSpace": 0,
+            "freeSpaceTimeStamp": "2017-06-26T13:26:58.5453916"
+        },
+        {
+            "id": 20,
+            "parkAbbrevId": "CACO",
+            "abbrevId": "LITTLECREEK",
+            "name": "Little Creek Parking (for Coast Guard Beach)",
+            "lattitude": 41.846383,
+            "longitude": -69.959873,
+            "capacity": 422,
+            "note": null,
+            "status": "Open",
+            "statusTimeStamp": "2017-06-26T13:26:58.545",
+            "freeSpace": 150,
+            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545"
+        },
+        {
+            "id": 21,
+            "parkAbbrevId": "CACO",
+            "abbrevId": "MARCONI",
+            "name": "Marconi",
+            "lattitude": 41.891549,
+            "longitude": -69.963428,
+            "capacity": 530,
+            "note": "",
+            "status": "Open",
+            "statusTimeStamp": "2017-06-26T13:26:58.5453916",
+            "freeSpace": 500,
+            "freeSpaceTimeStamp": "2017-06-26T13:26:58.5453916"
+        },
+        {
+            "id": 22,
+            "parkAbbrevId": "CACO",
+            "abbrevId": "HEADOFTHEMEADOW",
+            "name": "Head of the Meadow",
+            "lattitude": 42.051927,
+            "longitude": -70.082019,
+            "capacity": 285,
+            "note": "As of June 19, HEADMEADOW fee booth was still closed.",
+            "status": "Closed",
+            "statusTimeStamp": "2017-06-26T13:26:58.545",
+            "freeSpace": 0,
+            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545"
+        },
+        {
+            "id": 23,
+            "parkAbbrevId": "CACO",
+            "abbrevId": "RACEPOINT",
+            "name": "Race Point",
+            "lattitude": 42.079271,
+            "longitude": -70.218091,
+            "capacity": 292,
+            "note": "",
+            "status": "Closed",
+            "statusTimeStamp": "2017-06-26T13:26:58.5453916",
+            "freeSpace": 0,
+            "freeSpaceTimeStamp": "2017-06-26T13:26:58.5453916"
+        },
+        {
+            "id": 24,
+            "parkAbbrevId": "CACO",
+            "abbrevId": "HERRINGCOVE",
+            "name": "Herring Cove",
+            "lattitude": 42.044123,
+            "longitude": -70.216109,
+            "capacity": 602,
+            "note": "",
+            "status": "Closed",
+            "statusTimeStamp": "2017-06-26T13:26:58.5453916",
+            "freeSpace": 0,
+            "freeSpaceTimeStamp": "2017-06-26T13:26:58.5453916"
+        }
+    ];
+
+    showData(data);
+};
+
+function showData(data) {
+    data.forEach(function(element) {
+        var marker = L.marker([element.lattitude, element.longitude]).addTo(map);
+        console.log("marker", marker);
+        marker.bindPopup(createPopupContent(element));
+    });
+}
+
+function createPopupContent(lot) {
+
+    var lotStatus, lotCrowded, lastUpdated;
+
+    var lotFull = lot.capacity * 0.15;
+    var lotBusy = lot.capacity * 0.45;
+
+    if (lot.status == "Closed") {
+        lotStatus = "<p class='closedstatus'>Closed</p>";
+        lotCrowded = "";
+        lastUpdated = "<p class='lastupdated'>Last updated: " + moment(lot.statusTimeStamp, moment.ISO_8601).format("MMM D, h:m") + "</p>";
+    } else {
+        lotStatus = "<p class='openstatus'>Open</p>";
+        lotCrowded = "<p>Available Space: ";
+        if (lot.freeSpace <= lotFull) {
+            lotCrowded += "<span class='closedstatus'>None</span></p>";
+
+        } else if (lot.freeSpace <= lotBusy) {
+            lotCrowded += "<span class='crowdedstatus'>Limited</span></p>";
+        } else {
+            lotCrowded += "<span class='openstatus'>Yes</span></p>";
+        }
+        lastUpdated = "<p class='lastupdated'>Last updated: " + moment(lot.freeSpaceTimeStamp, moment.ISO_8601).format("MMM D, h:m") + "</p>";
+    }
+
+    return "<h1>" + lot.name + "</h1>" + lotStatus + lotCrowded + lastUpdated;
+}
