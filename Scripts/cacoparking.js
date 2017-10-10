4169a35ba771715fe26df0a2ddca5959419c4317
Brian Sumner <brian.sumner@dot.gov>
2017-10-10 12:46:59 -0400

    Switched from dummy data to API

--- Scripts/cacoparking.js
+++ Scripts/cacoparking.js
@@ -1,6 +1,6 @@
 const fullThreshold = 0.15;
 const limitedThreshold = 0.40;
-const apiUrl = "Insert URL Here";
+const apiUrl = "http://52.204.93.216/api/distributor/caco";
 
 function showData(data) {
     data.forEach(function (element) {
@@ -77,118 +77,9 @@ function lotIsLimited(lot) {
 }
 
 function fetchData(successFun, errorFun) {
-    // We use dummy data for now
-    var data = [
-        {
-            "id": 19,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "NAUSETLIGHT",
-            "name": "Nauset Light",
-            "lattitude": 41.858862,
-            "longitude": -69.951859,
-            "capacity": 167,
-            "note": null,
-            "status": "Open",
-            "statusTimeStamp": "2017-06-26T13:26:58.543",
-            "freeSpace": 10,
-            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpaceStatus": "Open"
-        },
-        {
-            "id": 20,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "LITTLECREEK",
-            "name": "Coast Guard - Satellite Parking",
-            "lattitude": 41.846383,
-            "longitude": -69.959873,
-            "capacity": 422,
-            "note": null,
-            "status": "Closed",
-            "statusTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpace": 0,
-            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpaceStatus": "Closed"
-        },
-        {
-            "id": 21,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "MARCONI",
-            "name": "Marconi",
-            "lattitude": 41.891549,
-            "longitude": -69.963428,
-            "capacity": 530,
-            "note": null,
-            "status": "Open",
-            "statusTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpace": 0,
-            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpaceStatus": "Full"
-        },
-        {
-            "id": 22,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "HEADOFTHEMEADOW",
-            "name": "Head of the Meadow",
-            "lattitude": 42.051927,
-            "longitude": -70.082019,
-            "capacity": 285,
-            "note": "As of June 19, HEADMEADOW fee booth was still closed.",
-            "status": "Open",
-            "statusTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpace": 0,
-            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpaceStatus": "Limited"
-        },
-        {
-            "id": 23,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "RACEPOINT",
-            "name": "Race Point",
-            "lattitude": 42.079271,
-            "longitude": -70.218091,
-            "capacity": 292,
-            "note": null,
-            "status": "Open",
-            "statusTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpace": 0,
-            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpaceStatus": "Open"
-        },
-        {
-            "id": 24,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "HERRINGCOVE",
-            "name": "Herring Cove",
-            "lattitude": 42.044123,
-            "longitude": -70.216109,
-            "capacity": 602,
-            "note": null,
-            "status": "Open",
-            "statusTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpace": 0,
-            "freeSpaceTimeStamp": "2017-06-26T13:26:58.545",
-            "freeSpaceStatus": "Full"
-        },
-        {
-            "id": 26,
-            "parkAbbrevId": "CACO",
-            "abbrevId": "COASTGUARD",
-            "name": "Coast Guard",
-            "lattitude": 2.0,
-            "longitude": 2.0,
-            "capacity": 400,
-            "note": null,
-            "status": "Open",
-            "statusTimeStamp": "2017-06-26T10:00:00",
-            "freeSpace": 20,
-            "freeSpaceTimeStamp": "2017-06-26T00:00:00",
-            "freeSpaceStatus": "Limited"
-        }
-    ];
-    successFun(data);
-    /*$.ajax({
-        url : dataUrl,
+    $.ajax({
+        url : apiUrl,
         success: successFun,
         error: errorFun
-    });*/
+    });
 }
